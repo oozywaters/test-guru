9bbff5641b1a4f050289b6a340159fad7ed268c4
oozwaters <oozy.waters@gmail.com>
2018-02-26 14:59:16 +0300

    Add answers null constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226112039) do
+ActiveRecord::Schema.define(version: 20180226115655) do
 
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct"
-    t.integer "question_id"
+    t.boolean "correct", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
