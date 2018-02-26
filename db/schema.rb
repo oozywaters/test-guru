7f028ccbb19018e4456687d57b053543ee830501
oozwaters <oozy.waters@gmail.com>
2018-02-26 15:06:09 +0300

    Add tests default level attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226120145) do
+ActiveRecord::Schema.define(version: 20180226120405) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20180226120145) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 1, null: false
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
