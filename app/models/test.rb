e97c989bb3e5d991edbd6e9793f6ad3724c1de3c
oozwaters <oozy.waters@gmail.com>
2018-03-01 14:20:08 +0300

    Add join table tests_users

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,4 @@
 class Test < ActiveRecord::Base
   belongs_to :category
+  has_and_belongs_to_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227055338) do
+ActiveRecord::Schema.define(version: 20180301110507) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20180227055338) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "test_id", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
