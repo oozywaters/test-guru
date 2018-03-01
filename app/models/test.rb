f7d09779cfed8fd46feddccdd70409d0d59ea94c
oozwaters <oozy.waters@gmail.com>
2018-03-01 15:56:54 +0300

    Add Test class method tests_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ActiveRecord::Base
   belongs_to :category
   has_and_belongs_to_many :users
+
+  def self.tests_by_category(category)
+    joins(:category)
+      .where(categories: { title: category })
+      .order(:title)
+      .pluck(:title)
+  end
 end
