999f03b6b41d94f7cb2c73dfa5d7e7ef95c6ee45
oozywaters <oozy.waters@gmail.com>
2018-03-02 22:49:53 +0300

    Add foreign keys to tests_users table

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20180302124800) do
     t.integer "test_id", null: false
     t.integer "user_id", null: false
     t.index ["test_id", "user_id"], name: "index_tests_users_on_test_id_and_user_id"
+    t.index ["test_id"], name: "index_tests_users_on_test_id"
+    t.index ["user_id"], name: "index_tests_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
