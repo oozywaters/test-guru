da3425411e34b5f855effa5b9405bae006624efe
oozwaters <oozy.waters@gmail.com>
2018-03-02 13:49:50 +0300

    Refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,42 +31,62 @@ categories = Category.create([
                                { title: 'Mobile Development'},
                                { title: 'Big Data' },
                                { title: 'Machine Learning' }
-                             ])
+                             ]).index_by(&:title)
 
 tests = Test.create([
-                      { title: 'Ruby', level: 1, category: categories[1] },
-                      { title: 'Ruby on Rails', level: 2, category: categories[1] },
-                      { title: 'Swift', level: 2, category: categories[2] },
-                      { title: 'JavaScript', level: 1, category: categories[0] },
-                      { title: 'React/Redux', level: 3, category: categories[0] }
-                    ])
+                      {
+                        title: 'Ruby',
+                        level: 1,
+                        category: categories.fetch('Backend')
+                      },
+                      {
+                        title: 'Ruby on Rails',
+                        level: 2,
+                        category: categories.fetch('Backend')
+                      },
+                      {
+                        title: 'Swift',
+                        level: 2,
+                        category: categories.fetch('Mobile Development')
+                      },
+                      {
+                        title: 'JavaScript',
+                        level: 1,
+                        category: categories.fetch('Frontend')
+                      },
+                      {
+                        title: 'React/Redux',
+                        level: 3,
+                        category: categories.fetch('Frontend')
+                      }
+                    ]).index_by(&:title)
 
 questions = Question.create([
                               {
                                 body: 'What is the difference between calling super and calling super()?',
-                                test: tests[0]
+                                test: tests.fetch('Ruby')
                               },
                               {
                                 body: 'Can you call a private method outside a Ruby class using its object?',
-                                test: tests[0]
+                                test: tests.fetch('Ruby')
                               },
                               {
                                 body: 'In Swift enumerations, what’s the difference between raw values and associated values?',
-                                test: tests[2]
+                                test: tests.fetch('Swift')
                               },
                               {
                                 body: 'What is NaN? What is its type? How can you reliably test if a value is equal to NaN?',
-                                test: tests[3]
+                                test: tests.fetch('JavaScript')
                               },
                               {
                                 body: 'What is JSX?',
-                                test: tests[4]
+                                test: tests.fetch('React/Redux')
                               }
                             ])
 
 Answer.create([
-                { text: 'Yes', question: questions[0] },
-                { text: 'No', question: questions[1] },
-                { text: 'Maybe', question: questions[2] },
-                { text: 'I dunno', question: questions[3] }
+                { text: 'Yes', question: questions.fetch(0) },
+                { text: 'No', question: questions.fetch(1) },
+                { text: 'Maybe', question: questions.fetch(2) },
+                { text: 'I dunno', question: questions.fetch(3) }
               ])
