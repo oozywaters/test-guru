7d4a693bcb4d77de14c033cd3f6e0e0aff039bb2
oozwaters <oozy.waters@gmail.com>
2018-03-23 19:37:02 +0300

    Refactor TestPassage model callbacks

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,11 +3,10 @@ class TestPassage < ActiveRecord::Base
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_current_question, on: %i[create update]
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-    self.current_question = next_questions.first
     save!
   end
 
@@ -43,10 +42,11 @@ class TestPassage < ActiveRecord::Base
   end
 
   def next_questions
+    return test.questions unless current_question
     test.questions.order(:id).where('id > ?', current_question.id)
   end
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    self.current_question = next_questions.first if test.present?
   end
 end
