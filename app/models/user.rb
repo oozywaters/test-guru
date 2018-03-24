636d3b553c318f65b64c266e619a01fd7572cd7d
oozywaters <oozy.waters@gmail.com>
2018-03-24 18:09:07 +0300

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
   validates :password, :email, presence: true
+  validates :email, format: EMAIL_FORMAT, uniqueness: true
 
   has_many :test_passages
   has_many :tests, through: :test_passages
