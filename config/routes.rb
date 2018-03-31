7ebddb47cc62256a3d7503453267b5b17b7bd25b
oozywaters <oozy.waters@gmail.com>
2018-03-31 13:34:53 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,10 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :tests, shallow: true do
-      resources :questions
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shallow: true, except: :index
+      end
     end
   end
 
