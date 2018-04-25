5fb9f239b76bd964191fa28fc5dd06b190e2aeb4
oozywaters <oozy.waters@gmail.com>
2018-04-25 23:34:10 +0300

    Add admin tests edit inline form

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -30,6 +30,14 @@ class Admin::TestsController < Admin::BaseController
       redirect_to [:admin, @test]
     else
       render :edit
+    end
+  end
+
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
     end
   end
 
