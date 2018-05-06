be3d05f68dd2674ff9b16352056a666e387f271f
oozywaters <oozy.waters@gmail.com>
2018-05-06 13:10:26 +0300

    Fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: 'ooz-test-guru.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'ooz-test-guru.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
@@ -72,8 +72,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test_guru_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { host: 'test-guru.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
